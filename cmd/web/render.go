4cb3cedaa8962bbdfb9c1b3a8c5d3f55262faf54
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-13 10:32:02 +0630

    update(render): template rendering utils func added

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"html/template"
 	"net/http"
-	"text/template"
+	"strings"
 )
 
 type templateData struct {
@@ -23,12 +25,55 @@ type templateData struct {
 var functions = template.FuncMap{}
 
 //go:embed templates
-var tempalteFS embed.FS
+var templateFS embed.FS
 
 func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
 	return td
 }
 
 func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	var t *template.Template
+	var err error
+	templateToRender := fmt.Sprintf("templates/%s.page.tmpl", page)
+	_, templateInMap := app.templateCache[templateToRender]
+	if app.config.env == "production" && templateInMap {
+		t = app.templateCache[templateToRender]
+	} else {
+		t, err = app.parseTemplate(partials, page, templateToRender)
+		if err != nil {
+			app.errorLog.Println(err)
+			return err
+		}
+	}
+	if td == nil {
+		td = &templateData{}
+	}
+	td = app.addDefaultData(td, r)
+	err = t.Execute(w, td)
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+	return nil
+}
 
+func (app *application) parseTemplate(partials []string, page, templateToRender string) (*template.Template, error) {
+	var t *template.Template
+	var err error
+	if len(partials) > 0 {
+		for i, x := range partials {
+			partials[i] = fmt.Sprintf("templates/%s.partials.tmpl", x)
+		}
+	}
+	if len(partials) > 0 {
+		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl", strings.Join(partials, ","), templateToRender)
+	} else {
+		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl", templateToRender)
+	}
+	if err != nil {
+		app.errorLog.Println(err)
+		return nil, err
+	}
+	app.templateCache[templateToRender] = t
+	return t, err
 }
