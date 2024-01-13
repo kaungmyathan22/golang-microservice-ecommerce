3a973e113b341914d9785daaa2160b701b808055
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-13 10:31:40 +0630

    update(hander): render template instead of printing to console.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,5 +3,7 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("it handler")
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
 }
